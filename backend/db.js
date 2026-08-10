18b66049be272f625f60f3ad7bd72c8ed5ff6a45
Taehyeok Kwon <tkwon12@terpmail.umd.edu>
2026-08-10 13:44:26 -0400

    Add production start script

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const {Pool} = require('pg');
 require("dotenv").config();
 
-const pool = new Pool({
+const pool = process.env.DATABASE_URL? new Pool({ connectionString:process.env.DATABASE_URL,
+    ssl:{rejectUnauthorized:false}}) : new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
